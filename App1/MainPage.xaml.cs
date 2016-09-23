7f04f5669475050c11093e0b02304cbbdf9a062f
joe dean <joe@joedean.net>
2016-09-23 17:06:50 +0800

    added media player and button

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +28,40 @@ namespace App1
         public MainPage()
         {
             this.InitializeComponent();
+            mediaPlayer.MediaFailed += mediaPlayer_MediaFailed;
+        }
+
+        private async void mediaPlayer_MediaFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            var dialog = new MessageDialog(e.ErrorMessage);
+            await dialog.ShowAsync();
+        }
+
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await SetLocalMedia();
+        }
+
+        async private System.Threading.Tasks.Task SetLocalMedia()
+        {
+            var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
+
+            openPicker.FileTypeFilter.Add(".wmv");
+            openPicker.FileTypeFilter.Add(".mp4");
+            openPicker.FileTypeFilter.Add(".wma");
+            openPicker.FileTypeFilter.Add(".mp3");
+
+            var file = await openPicker.PickSingleFileAsync();
+
+            // mediaPlayer is a MediaElement defined in XAML
+            if (file != null)
+            {
+                var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                mediaPlayer.SetSource(stream, file.ContentType);
+
+                mediaPlayer.Play();
+            }
         }
     }
 }
